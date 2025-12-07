a58c7b53eadec611d30d49b29031bb297e1b9186
Daffy Hatzidis <triantafyllosxatz@gmail.com>
2025-12-07 12:26:31 +0200

    gay

--- src/main/java/com/hua/app/App.java
+++ src/main/java/com/hua/app/App.java
@@ -5,6 +5,6 @@ package com.hua.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!8=>");
     }
 }
