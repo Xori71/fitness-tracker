48e6f606dcedc6ae21caac7c20a74259939cd63f
Xori71 <alexandrosgeorgiou35@gmail.com>
2026-01-05 15:59:06 +0200

    Starter UI

---
+++ src/main/java/com/hua/app/utilities/userinterface/FilePicker.java
@@ -0,0 +1,111 @@
+package com.hua.app.utilities.userinterface;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+public class FilePicker {
+    private static Set<File> fileList = new HashSet<>();
+    
+    public static JScrollPane instantiateFilePicker(JPanel canvas) {
+        JPanel filePickerPanel = new JPanel(true);
+        filePickerPanel.setLayout(new BoxLayout(filePickerPanel, BoxLayout.Y_AXIS));
+        setupFilePicker(filePickerPanel, canvas);
+        
+        /* Make button column scrollable if it exceeds maximum height */
+        JScrollPane scrollPanel = new JScrollPane(filePickerPanel);
+        scrollPanel.getViewport().setBackground(Color.GRAY);
+        scrollPanel.setPreferredSize(new Dimension(350, 300));
+        scrollPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        /* Remove ugly line border */
+        scrollPanel.setBorder(BorderFactory.createEmptyBorder());
+        
+        return scrollPanel;
+    }
+    
+    public static Set<File> getFileList() {
+        return fileList;
+    }
+    
+    private static void setupFilePicker(JPanel filePickerPanel, JPanel canvas) {
+        JButton button = new JButton("Choose a file");
+        button.setFocusPainted(false);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        /**
+         * This variable is for checking whether a file-picking button has already been clicked at least
+         * once. Since I allow selection editing, the recursive nature of the button creation means that,
+         * without this state holder, each edit will create a new "Choose a file" button, even if another
+         * already exists.
+         * 
+         * [Choose a file] -> [Chosen file] (Edit the selection) -> [New chosen file]
+         *                    [Chose a file]                        [Choose a file]
+         *                                                          [Choose a file]
+         * 
+         * AtomicBoolean might seem like overkill for this job, but since lambda expressions cannot modify variables
+         * that are not 'final' (and I want a state holder in *each* button), the usage of a 'type' that creates a
+         * new object with each iteration is convenient.
+         */
+        AtomicBoolean hasBeenClicked = new AtomicBoolean(false);
+        
+        button.addActionListener(l -> {
+            /* The button being clicked */
+            JButton source = (JButton) l.getSource();
+            /* Hold the previous file in case of editing */
+            File previousSelection = (File) source.getClientProperty("SelectedFile");
+            
+            JFileChooser filePicker = new JFileChooser();
+            if (filePicker.showOpenDialog(canvas) == JFileChooser.APPROVE_OPTION) {
+                File selection = filePicker.getSelectedFile();
+                String filename = selection.getName();
+                int dotIndex = filename.lastIndexOf('.');
+                String extension = (dotIndex > 0) ? filename.substring(dotIndex + 1) : "";
+                if (extension.equals("tcx")) {
+                    if (!fileList.contains(selection)) {
+                        if (hasBeenClicked.get() == true && previousSelection != null) {
+                            /* Remove old file after editing selection */
+                            fileList.remove(previousSelection);
+                        }
+                        fileList.add(selection);
+                        /* Save new selection into button */
+                        source.putClientProperty("SelectedFile", selection);
+                        button.setText(filename);
+                        if (hasBeenClicked.get() == false) {
+                            addNewFileButton(filePickerPanel, canvas);
+                            hasBeenClicked.set(true);
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "You have already selected this file");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "This file format is unsupported");
+                }
+            }
+        });
+        
+        filePickerPanel.add(button);
+        /* Add space between buttons */
+        filePickerPanel.add(Box.createRigidArea(new Dimension(1, 10)));
+    }
+
+	private static void addNewFileButton(JPanel filePickerPanel, JPanel canvas) {
+	    setupFilePicker(filePickerPanel, canvas);
+		/* For dynamic updating */
+		filePickerPanel.revalidate();
+		filePickerPanel.repaint();
+	}
+}
---
+++ src/main/java/com/hua/app/utilities/userinterface/MiscellaneousInfo.java
@@ -0,0 +1,91 @@
+package com.hua.app.utilities.userinterface;
+
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.text.NumberFormat;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.text.NumberFormatter;
+
+public class MiscellaneousInfo {
+    private static JFormattedTextField ageField;
+    private static JComboBox<String> selector;
+    private static JFormattedTextField weightField;
+    
+    public static JPanel instantiateInfoPicker(JPanel canvas) {
+        /* Base panel for all the info */
+        JPanel infoPanel = new JPanel(true);
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.add(setupAgeInput());
+        infoPanel.add(setupWeightInput());
+        infoPanel.add(setupSexInput());
+        
+        return infoPanel;
+    }
+    
+    public static int getAgeInput() {
+        return (int) ageField.getValue();
+    }
+    
+    public static double getWeightInput() {
+        return (double) weightField.getValue();
+    }
+    
+    public static String getSexInput() {
+        return (String) selector.getSelectedItem();
+    }
+    
+    private static JComboBox<String> setupSexInput() {
+        selector = new JComboBox<>();
+        selector.addItem("-");
+        selector.addItem("Male");
+        selector.addItem("Female");
+        
+        return selector;
+	}
+
+	private static Component setupWeightInput() {
+	    JPanel weightPanel = new JPanel(new FlowLayout(), true);
+        weightPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        NumberFormat weightFormat = NumberFormat.getNumberInstance();
+        weightFormat.setMinimumFractionDigits(0);
+        weightFormat.setMaximumFractionDigits(2);
+        NumberFormatter weightFormatter = new NumberFormatter(weightFormat);
+        weightFormatter.setValueClass(Double.class);
+        weightFormatter.setMinimum(0);
+        weightFormatter.setMaximum(600);
+        weightFormatter.setAllowsInvalid(true);
+        weightField = new JFormattedTextField(weightFormatter);
+        weightField.setValue(0.0);
+        weightField.setColumns(6);
+        
+        weightPanel.add(new JLabel("Enter your weight (optional): "));
+        weightPanel.add(weightField);
+        
+        return weightPanel;
+	}
+
+	private static JPanel setupAgeInput() {
+        JPanel agePanel = new JPanel(new FlowLayout(), true);
+        agePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        NumberFormat ageFormat = NumberFormat.getIntegerInstance();
+        NumberFormatter ageFormatter = new NumberFormatter(ageFormat);
+        ageFormatter.setValueClass(Integer.class);
+        ageFormatter.setMinimum(0);
+        ageFormatter.setMaximum(122);
+        ageFormatter.setAllowsInvalid(true);
+        ageField = new JFormattedTextField(ageFormatter);
+        ageField.setValue(0);
+        ageField.setColumns(3);
+        
+        agePanel.add(new JLabel("Enter your age (optional): "));
+        agePanel.add(ageField);
+        
+        return agePanel;
+    }
+}
