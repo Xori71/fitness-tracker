70ecdd6662ea326734a8dba77d7327811399faa5
Xori71 <alexandrosgeorgiou35@gmail.com>
2026-01-19 14:07:02 +0200

    Catch a small logical error in the advanced calorie calculation formula

--- src/main/java/com/hua/app/data/calories/ComplexFormula.java
+++ src/main/java/com/hua/app/data/calories/ComplexFormula.java
@@ -12,10 +12,22 @@ public class ComplexFormula implements Formula{
     
     @Override
     public double calculate(Activity activity) {
+        double calories;
         if (data.getSex().equals("m")) {
-            return (-55.0969 + (0.6309 * activity.getAverageHeartRate()) + (0.1966 * data.getWeight()) + (0.2017 * data.getAge())) * (activity.getDurationInSeconds() / 60.0) / 4.184;
+            calories = (-55.0969 + (0.6309 * activity.getAverageHeartRate()) + (0.1966 * data.getWeight()) + (0.2017 * data.getAge())) * (activity.getDurationInSeconds() / 60.0) / 4.184;
         } else {
-            return (-20.4022 + (0.4472 * activity.getAverageHeartRate()) + (0.1263 * data.getWeight()) + (0.074 * data.getAge())) * (activity.getDurationInSeconds() / 60.0) / 4.184;
+            calories = (-20.4022 + (0.4472 * activity.getAverageHeartRate()) + (0.1263 * data.getWeight()) + (0.074 * data.getAge())) * (activity.getDurationInSeconds() / 60.0) / 4.184;
+        }
+        
+        /* If there is zero-value data in the above formula, the calculation may break
+           down and result in a negative expenditure. To prevent this logical error, 
+           I treat negative calorie results like it was impossible to calculate 
+           the expenditure for the respective activity, based on the data the program
+           was given, and return 0 */
+        if (calories < 0) {
+            return 0.0;
+        } else {
+            return calories;
         }
     }
 }
